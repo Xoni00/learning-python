6dde64b4beb050a694abac0131b06f552192ebef
Damian <84410871+Xoni00@users.noreply.github.com>
2025-07-16 00:32:11 +0200

    Add programs

---
+++ bid.py
@@ -0,0 +1,27 @@
+auction = {
+    
+}
+print("Welcome to the secret auction program.")
+
+
+def auction_game():
+    name = input("What's your name?: ")
+    bid = int(input("What's your bid?: "))
+    auction[name] = bid
+    
+
+game = True
+
+while game:
+    auction_game()
+    decision = input("Are there other biders? Type 'yes' or 'no'\n")
+    if decision == 'yes':
+        print('\n')
+    else:
+        game = False
+        
+
+max_key = max(auction, key=auction.get)
+max_value = auction[max_key]
+
+print(f"The winner is {max_key} with the bid of ${max_value}")
---
+++ hangman.py
@@ -0,0 +1,59 @@
+import random 
+
+
+word_list = [
+    "gwóźdź",
+    "ogień",
+    "algorytm",
+    "cisza",
+    "grzyb",
+    "kod",
+    "próżnia",
+    "błysk",
+    "rytuał",
+    "wiatrak",
+    "chciwość",
+    "świt",
+    "rekurencja",
+    "portal",
+    "czaszka",
+    "pokusa",
+    "równowaga",
+    "klątwa",
+    "przestrzeń",
+    "pamięć"
+]
+
+random_word = random.choice(word_list)
+#? print("".join(random_word))
+
+#!zamiana liter na "_"
+placeholder = []
+for _ in random_word:
+    placeholder += "_"
+print(*placeholder)
+
+
+life = 6
+#! sprawdzanie czy litera jest w słowie
+while life > 0:
+    guess  = str(input("guess a letter: "))
+    if guess not in random_word:
+        life -= 1
+        print(f"źle, pozostało {life} żyć")
+        print(*placeholder)
+    else:
+        for i in range(len(random_word)):
+            if guess == random_word[i]:
+                placeholder[i] = guess
+        print(''.join(placeholder))
+    if "".join(placeholder) == random_word:
+        print("wygrałeś")
+        break 
+else:
+    print(f"Przegrałeś zgadywane słowo to: {random_word} ")
+
+
+
+
+
