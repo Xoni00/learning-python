77b7db3619d2885fa89e5b9f9c9f593b42599fb1
Damian <84410871+Xoni00@users.noreply.github.com>
2025-07-22 01:49:28 +0200

    Refactor Blackjack game logic and improve scoring mechanism

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,7 @@
 import random
 
+SCORE_LIMIT = 21
+
 card_list = [
     "2",
     "3",
@@ -13,7 +15,7 @@ card_list = [
     "J",
     "Q",
     "K",
-] * 4
+]
 
 cards = {
     "2": 2,
@@ -28,35 +30,88 @@ cards = {
     "J": 10,
     "Q": 10,
     "K": 10,
-    "A": [1, 11],  # As może mieć 1 lub 11
+    # "A": [1, 11],  # As może mieć 1 lub 11
 }
+
+
+def result():
+    if another_card == "n" and card_score > computer_card_score:
+        print(f"Your final hand: {player_cards}, final score: {card_score}")
+        print(
+            f"Computer's final hand: {computer_cards}, final score: {computer_card_score}"
+        )
+        print("You win")
+    elif card_score == computer_card_score:
+        print(f"Your final hand: {player_cards}, final score: {card_score}")
+        print(
+            f"Computer's final hand: {computer_cards}, final score: {computer_card_score}"
+        )
+        print("draw")
+    elif computer_card_score == 21:
+        print(f"Your final hand: {player_cards}, final score: {card_score}")
+        print(
+            f"Computer's final hand: {computer_cards}, final score: {computer_card_score}"
+        )
+        print("Computer has a Blackjack, You loose")
+    else:
+        print(f"Your final hand: {player_cards}, final score: {card_score}")
+        print(
+            f"Computer's final hand: {computer_cards}, final score: {computer_card_score}"
+        )
+        print("You loose")
+
+
 #! jesli krupier ma mniej niz 17 pkt w kartach musi ciagnac kolejna karte
+decision = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+if decision != "y":
+    exit
 
-# while True:
-
-#     decision = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-#     if decision != "y":
-#         break
-
+#! player first cards
 player_cards = []
 
 for _ in range(2):
-    player_cards += random.choice(card_list)
-
-card_score = 0
-
-for card in player_cards:
-    card_score += cards.get(card)
-
-computer_firs_card = random.choice(card_list)
-
-print(f"Your cards: {player_cards}, current score: {card_score}")
-print(f"Computer`s first card: {computer_firs_card}")
-
-another_card = input("Type 'y' to get another card, type 'n' to pass: ")
-
-if another_card == "y":
     player_cards.append(random.choice(card_list))
-else:
-    print(f"Your final hand: {player_cards}, final score: {card_score}")
-    print()
+
+#! Computer cards and cards score
+computer_cards = []
+for _ in range(2):
+    computer_cards.append(random.choice(card_list))
+
+computer_card_score = 0
+
+for card in computer_cards:
+    computer_card_score += cards.get(card)
+
+while True:
+    #! player cards score
+
+    card_score = 0
+
+    for card in player_cards:
+        card_score += cards.get(card)
+
+    if card_score > SCORE_LIMIT:
+        print(f"Your final hand: {player_cards}, final score: {card_score}")
+        print(
+            f"Computer's final hand: {computer_cards}, final score: {computer_card_score}"
+        )
+        print("You went over. You loose")
+        break
+
+    print(f"Your cards: {player_cards}, current score: {card_score}")
+
+    if computer_card_score == SCORE_LIMIT:
+        print(
+            f"Computer's have a BlackJack!{computer_cards}, score: {computer_card_score}"
+        )
+    else:
+        print(f"Computer`s first card: {computer_cards[0]}")
+
+    #! draw another card
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+
+    if another_card == "y" and card_score < SCORE_LIMIT:
+        player_cards.append(random.choice(card_list))
+    else:
+        result()
+        break
