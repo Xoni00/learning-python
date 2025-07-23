e45b5d6221902c75ebe26126ebca0a9d03d4fb34
Damian <84410871+Xoni00@users.noreply.github.com>
2025-07-23 03:11:35 +0200

    Refactor number guessing game logic for clarity and fix game over conditions

--- the_number_guessing_game.py
+++ the_number_guessing_game.py
@@ -7,44 +7,56 @@ print("I'm thinking of a number between 1 and 100.")
 random_number = random.randint(1, 100)
 print(random_number)
 
+game = True
+
 
 def easy_game():
+    global game
+
     print(f"You have {game_mode["easy_mode"]} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
 
     if game_mode["easy_mode"] == 0:
         print("You loose")
-    guess = int(input("Make a guess: "))
-    if guess > random_number:
+        game = False
+    elif guess > random_number:
         print("Too high")
         game_mode["easy_mode"] -= 1
     elif guess == random_number:
         print("Correct.")
+        game = False
     else:
         print("Too low.")
         game_mode["easy_mode"] -= 1
 
 
 def hard_game():
+    global game
     print(f"You have {game_mode["hard_mode"]} attempts remaining to guess the number.")
-
-    if game_mode["hard_mode"] == 0:
-        print("You loose")
     guess = int(input("Make a guess: "))
-    if guess > random_number:
+
+    if game_mode["hard_mode"] <= 0:
+        print("You loose")
+        game = False
+    elif guess > random_number:
         print("Too high")
         game_mode["hard_mode"] -= 1
     elif guess == random_number:
         print("Correct.")
+        game = False
     else:
         print("Too low.")
         game_mode["hard_mode"] -= 1
 
 
-while True:
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+while game:
 
     match difficulty:
         case "easy":
             easy_game()
         case "hard":
             hard_game()
+        case _:
+            print("error")
