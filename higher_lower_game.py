322f5c6b7a0bc6e4d65e0b5f99a225946d5b7278
Damian <84410871+Xoni00@users.noreply.github.com>
2025-07-25 03:04:03 +0200

    Refactor higher-lower game logic for improved readability and correct user prompts

--- higher_lower_game.py
+++ higher_lower_game.py
@@ -2,9 +2,9 @@ import random
 
 DATA = {
     "Facebook": 55000000,
-    "Tesla": 41000000,
-    "Cristiano Ronaldo": 67000000,
-    "Bitcoin": 34000000,
+    "Disney": 41000000,
+    "Telegram": 67000000,
+    "Signal": 34000000,
     "Instagram": 49000000,
     "Google": 80000000,
     "Netflix": 32000000,
@@ -15,23 +15,28 @@ DATA = {
 
 score = 0
 
-first_key = random.choice(list(DATA))
-print(f"Compare A: {first_key}: {DATA[first_key]}")
 
-second_key = random.choice(list(DATA))
-print(f"Compare B: {second_key}: {DATA[second_key]}")
+while True:
+    print(f"Actuall score: {score}")
 
-guess = input("Who has more followers? Type 'A' or 'B'").lower()
+    first_key = random.choice(list(DATA))
+    print(f"Compare A: {first_key}: {DATA[first_key]}")
 
+    second_key = random.choice(list(DATA))
+    print(f"Compare B: {second_key}: {DATA[second_key]}")
 
-is_a_correct = guess == "a" and DATA[first_key] > DATA[second_key]
-is_b_correct = guess == "b" and DATA[first_key] < DATA[second_key]
+    guess = input("Who has more Users? Type 'A' or 'B'").lower()
 
-if is_a_correct or is_b_correct:
-    score += 1
-    print(f"You're right! Current score: {score}")
-elif DATA[first_key] == DATA[second_key]:
-    print("Draw")
-else:
-    print(f"Sorry that's wrong. Final score: {score}")
-    score = 0
+    is_a_correct = guess == "a" and DATA[first_key] > DATA[second_key]
+    is_b_correct = guess == "b" and DATA[first_key] < DATA[second_key]
+
+    if is_a_correct or is_b_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    elif DATA[first_key] == DATA[second_key]:
+        print("Draw")
+    else:
+        print(f"Sorry that's wrong. Final score: {score}")
+        score = 0
+        break
+    print("\n" * 2)
