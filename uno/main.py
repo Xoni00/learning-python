517b0411cd88ef164aa78c7e8bf03dd73a04dc3d
Damian <84410871+Xoni00@users.noreply.github.com>
2025-09-03 02:31:45 +0200

    Refactor deck initialization to use AVAILABLE_CARDS and remove redundant card definitions

--- uno/main.py
+++ uno/main.py
@@ -1,8 +1,7 @@
 from cards import AVAILABLE_CARDS
+import copy
 
 import random
-
-COLORS = ["red", "yellow", "green", "blue", "black"]
 
 
 def cards(color, value, kind):
@@ -14,55 +13,7 @@ def cards(color, value, kind):
     return card
 
 
-deck = []
-
-
-#! number zero cards
-deck.append(cards("red", 0, None))
-deck.append(cards("yellow", 0, None))
-deck.append(cards("green", 0, None))
-deck.append(cards("blue", 0, None))
-
-#! 1-9 cards
-for i in range(1, 10):
-    for _ in range(2):
-        deck.append(cards("red", i, None))
-        deck.append(cards("yellow", i, None))
-        deck.append(cards("green", i, None))
-        deck.append(cards("blue", i, None))
-
-#! stop cards
-for i in range(2):
-    deck.append(cards("red", None, "stop"))
-    deck.append(cards("yellow", None, "stop"))
-    deck.append(cards("green", None, "stop"))
-    deck.append(cards("blue", None, "stop"))
-
-#! change direction icards
-for i in range(2):
-    deck.append(cards("red", None, "change_direction"))
-    deck.append(cards("yellow", None, "change_direction"))
-    deck.append(cards("green", None, "change_direction"))
-    deck.append(cards("blue", None, "change_direction"))
-
-#! +2 cards
-for i in range(2):
-    deck.append(cards("red", None, "+2"))
-    deck.append(cards("yellow", None, "+2"))
-    deck.append(cards("green", None, "+2"))
-    deck.append(cards("blue", None, "+2"))
-
-#! +4 cards
-deck.append(cards("red", None, "+4"))
-deck.append(cards("yellow", None, "+4"))
-deck.append(cards("green", None, "+4"))
-deck.append(cards("blue", None, "+4"))
-
-#! change color cards
-deck.append(cards("red", None, "change_color"))
-deck.append(cards("yellow", None, "change_color"))
-deck.append(cards("green", None, "change_color"))
-deck.append(cards("blue", None, "change_color"))
+deck = copy.deepcopy(AVAILABLE_CARDS)
 
 players = []
 
@@ -84,10 +35,8 @@ while True:
         for player in players:
             for card in range(7):
                 player["hand"].append(deck[card])
-                deck.pop(i)
+                deck.pop(card)
 
-    # if card["value"] is not None:
-    #     return f"{card["color"].capitalize()} {card["value"]}"
-    # else:
-    #     return f"{card["color"].capitalize()} {card["kind"]}"
+    print(len(deck))
+
     break
